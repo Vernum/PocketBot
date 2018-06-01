290428a2ff87e4eaaa044778e056623786ebdbe6
Donny Smith <Vernum.ds@gmail.com>
2018-06-01 15:36:20 +0930

    sdfg
    sdfg

--- bot.go
+++ bot.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"flag"
+)
+
+var (
+	fDiscord bool
+	fIRC     bool
+	dToken   string
+	iToken   string
+)
+
+func init() {
+	flag.BoolVar(&fDiscord, "de", false, "Turn on the Discord Bot")
+	flag.StringVar(&dToken, "t", "", "Discord Token")
+	flag.BoolVar(&fIRC, "ie", false, "Enable IRC bot")
+	flag.StringVar(&iToken, "it", "", "Twitch OAuth token")
+	flag.Parse()
+}
+
 func main() {
-    
+	if fDiscord {
+		runDiscord()
+	}
 }
